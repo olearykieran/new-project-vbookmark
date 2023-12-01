5fccb3d61259fa40d998fba3839f1dab4e79b6ed
olearykieran <koleary515@gmail.com>
2023-11-30 23:54:09 -0400

    workingggg need to fix timestamp.

--- ios/NewProject/AppDelegate.mm
+++ ios/NewProject/AppDelegate.mm
@@ -2,16 +2,30 @@
 
 #import <React/RCTBundleURLProvider.h>
 
-
+#import <Firebase.h>
 
 @implementation AppDelegate
+
   
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  @try {
+      [FIRApp configure];
+      NSLog(@"Firebase configured successfully.");
+    }
+    @catch (NSException *exception) {
+      NSLog(@"Error configuring Firebase: %@", exception.reason);
+    }
+  
   self.moduleName = @"NewProject";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  // Add the print statement here
+  NSDictionary* infoDict = [[NSBundle mainBundle] infoDictionary];
+  NSString* storyboardName = infoDict[@"NSExtensionMainStoryboard"];
+  NSLog(@"Main storyboard: %@", storyboardName);
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
--- ios/Youtube Share/ShareViewController.swift
+++ ios/Youtube Share/ShareViewController.swift
@@ -3,81 +3,212 @@ import Social
 import MobileCoreServices
 import UniformTypeIdentifiers
 import Firebase
+import Alamofire
+
+struct YoutubeVideo {
+    let title: String
+    // Add other properties as needed
+}
 
 class ShareViewController: UIViewController {
+    // URL to be processed
+    var sharedURL: URL?
   
-  
+    let apiKey = ProcessInfo.processInfo.environment["YOUTUBE_API_KEY"] ?? ""
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-  // This function is called when the Post button is pressed
-      @IBAction func postButtonPressed(_ sender: UIButton) {
-          didSelectPost()
+        // Configure Firebase once when the view loads
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+            print("Firebase configured in Share Extension.")
+        } else {
+            print("Firebase already configured.")
+        }
+
+        // Check for shared content immediately when the view is loaded
+        if let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            processSharedContent(extensionItem)
+        }
+    }
+
+  private func processSharedContent(_ extensionItem: NSExtensionItem) {
+      if let itemProviders = extensionItem.attachments {
+          for itemProvider in itemProviders {
+              print("Registered type identifiers: \(itemProvider.registeredTypeIdentifiers)")
+
+              if itemProvider.hasItemConformingToTypeIdentifier(UTType.plainText.identifier) {
+                  itemProvider.loadItem(forTypeIdentifier: UTType.plainText.identifier, options: nil) { [weak self] (item, error) in
+                      DispatchQueue.main.async {
+                          if let text = item as? String {
+                              print("Plain text received: \(text)")
+                              self?.handlePlainText(text)
+                          } else if let error = error {
+                              print("Error loading plain text item: \(error)")
+                          }
+                      }
+                  }
+              } else {
+                  print("Item provider does not conform to expected type identifiers")
+              }
+          }
+      } else {
+          print("No item providers found in extension item")
       }
+  }
 
-      // This function is called when the Cancel button is pressed
-      @IBAction func cancelButtonPressed(_ sender: UIButton) {
-          extensionContext?.cancelRequest(withError: NSError())
+  private func handlePlainText(_ text: String) {
+      if let url = extractURL(from: text) {
+          print("Extracted URL: \(url)")
+          sharedURL = url
+          extractAndStoreTimestamp(from: url)
+          // Further processing as needed
+      } else {
+          print("No URL found in plain text")
       }
+  }
+
+  private func extractURL(from text: String) -> URL? {
+      // Basic URL extraction from text
+      // This can be enhanced based on the expected format of the text
+      return URL(string: text.trimmingCharacters(in: .whitespacesAndNewlines))
+  }
+
+  private func extractAndStoreTimestamp(from url: URL) {
+      let queryItems = URLComponents(string: url.absoluteString)?.queryItems
+      let timeParameter = queryItems?.first(where: { $0.name == "t" })?.value
+
+      // Convert the time parameter to seconds
+      let timeInSeconds = convertYouTubeTimeParameterToSeconds(timeParameter)
+      print("Extracted Timestamp: \(timeInSeconds) seconds")
+      // Store the timestamp for later use
+      // ...
+  }
+
+  private func convertYouTubeTimeParameterToSeconds(_ timeParameter: String?) -> Int {
+      guard let time = timeParameter else { return 0 }
+
+      let timeComponents = time.components(separatedBy: CharacterSet.decimalDigits.inverted)
+      let timeNumbers = timeComponents.compactMap { Int($0) }
+
+      switch timeNumbers.count {
+      case 1: // Only seconds
+          return timeNumbers[0]
+      case 2: // Minutes and seconds
+          return timeNumbers[0] * 60 + timeNumbers[1]
+      default:
+          return 0
+      }
+  }
+
+    @IBAction func postButtonPressed(_ sender: UIButton) {
+        if let url = sharedURL {
+            saveBookmarkToFirebase(url: url)
+        }
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        let error = NSError(domain: "com.holygrail.YoutubeShareExtensionError", code: 1001, userInfo: [NSLocalizedDescriptionKey: "User cancelled the action"])
+        extensionContext?.cancelRequest(withError: error)
+    }
 
     func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of content here.
-        FirebaseApp.configure()
-        if let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem {
-            if let itemProviders = extensionItem.attachments {
-                for itemProvider in itemProviders {
-                    if itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
-                        itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil, completionHandler: { (item, error) in
-                            if let url = item as? URL {
-                                print("URL received: \(url)")
-                                self.saveBookmarkToFirebase(url: url)
-                            } else if let error = error {
-                                print("Error loading item: \(error)")
-                            }
-                        })
-                    }
-                }
-            }
-        }
-        
-        // Signal that we are done with the request
         extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
-    
-  private func saveBookmarkToFirebase(url: URL) {
-      // Retrieve the 'userID' from UserDefaults
-      if let userDefaults = UserDefaults(suiteName: "group.com.holygrail.bookmark") {
-          if let userID = userDefaults.string(forKey: "userID") {
-              // If userID is found, proceed to save the bookmark
-              let bookmarksCollectionRef = Firestore.firestore().collection("Bookmarks")
-              
-              // Extract the thumbnail, time, and title from the URL if possible or set defaults
-              let thumbnail = "https://img.youtube.com/vi/\(url.lastPathComponent)/0.jpg"
-              let time = 0.0 // Replace with actual time if available
-              let title = "Video Title" // Replace with actual title if you can extract it
-              
-              // Creating the bookmark data dictionary
-              let bookmarkData: [String: Any] = [
-                  "created": Timestamp(date: Date()),
-                  "thumbnail": thumbnail,
-                  "time": time,
-                  "title": title,
-                  "url": url.absoluteString,
-                  "userID": userID  // Use the userID here
-              ]
-              
-              // Add the bookmark to Firestore
-              bookmarksCollectionRef.addDocument(data: bookmarkData) { error in
-                  if let error = error {
-                      print("Error saving bookmark: \(error)")
-                  } else {
-                      print("Bookmark saved successfully")
-                  }
+
+    private func saveBookmarkToFirebase(url: URL) {
+        guard let userDefaults = UserDefaults(suiteName: "group.com.holygrail.bookmark"),
+              let userID = userDefaults.string(forKey: "userID") else {
+            print("UserID not found in UserDefaults")
+            return
+        }
+        print("User ID retrieved: \(userID)")
+
+        let videoID = extractYoutubeVideoID(from: url)
+        fetchYoutubeVideoDetails(videoID: videoID) { [weak self] result in
+            print("Fetch YouTube Video Details Result: \(result)")
+            switch result {
+            case .success(let video):
+                let thumbnail = "https://img.youtube.com/vi/\(videoID)/0.jpg"
+                let bookmarkData: [String: Any] = [
+                    "created": Timestamp(date: Date()),
+                    "thumbnail": thumbnail,
+                    "time": 0.0, // Replace with actual time if available
+                    "title": video.title, // Using fetched title
+                    "url": url.absoluteString,
+                    "userID": userID
+                ]
+                self?.saveToFirebase(data: bookmarkData) {
+                                self?.didSelectPost()
+                            }
+            case .failure(let error):
+                print("Error fetching video details: \(error)")
+                self?.didSelectPost()
+            }
+        }
+    }
+
+    private func saveToFirebase(data: [String: Any], completion: @escaping () -> Void) {
+        print("Entering saveToFirebase with data: \(data)")
+        let bookmarksCollectionRef = Firestore.firestore().collection("Bookmarks")
+        bookmarksCollectionRef.addDocument(data: data) { error in
+            if let error = error {
+                print("Error saving bookmark: \(error)")
+            } else {
+                print("Bookmark saved successfully")
+            }
+            completion()
+        }
+    }
+  private func fetchYoutubeVideoDetails(videoID: String, completion: @escaping (Result<YoutubeVideo, Error>) -> Void) {
+      print("Starting to fetch video details for ID: \(videoID)")
+      let url = "https://www.googleapis.com/youtube/v3/videos?id=\(videoID)&key=\(apiKey)&part=snippet,contentDetails"
+      print("Sending request to: \(url)")
+
+      AF.request(url).response { response in
+          print("Response received for video details request")
+          print("Raw response: \(String(describing: response.data))")
+          switch response.result {
+          case .success(let data):
+              guard let jsonData = data else {
+                  print("No data received")
+                  completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                  return
               }
-          } else {
-              // Handle the case where userID is not found
-              print("UserID not found in UserDefaults")
+
+              do {
+                  let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any]
+                  if let items = json?["items"] as? [[String: Any]],
+                     let firstItem = items.first,
+                     let snippet = firstItem["snippet"] as? [String: Any],
+                     let title = snippet["title"] as? String {
+                      let video = YoutubeVideo(title: title)
+                      print("Video title: \(title)")
+                      completion(.success(video))
+                  } else {
+                      print("Items or title not found in JSON")
+                      completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Items or title not found"])))
+                  }
+              } catch {
+                  print("JSON parsing error: \(error)")
+                  completion(.failure(error))
+              }
+
+          case .failure(let error):
+              print("Network request failed: \(error)")
+              completion(.failure(error))
           }
       }
   }
-    // Other necessary overrides...
+
+
+    private func extractYoutubeVideoID(from url: URL) -> String {
+        if let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems {
+            for item in queryItems where item.name == "v" {
+                return item.value ?? ""
+            }
+        }
+        return url.lastPathComponent
+    }
 }
